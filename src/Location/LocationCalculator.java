e19edd71cb91768f14f5ceeba569bb679cb97fb9
Bram van Vliet <g.j.b.vanvliet@student.utwente.nl>
2017-04-05 12:14:03 +0200

    first implementation of the fairWeightCalculation

--- src/Location/LocationCalculator.java
+++ src/Location/LocationCalculator.java
@@ -2,6 +2,7 @@ package Location;
 
 import Utils.MacRssiPair;
 import Utils.Position;
+import Utils.Utils;
 
 import java.util.LinkedList;
 
@@ -55,18 +56,15 @@ public class LocationCalculator {
         }
     }
 
-    /*public static Position fairPositionCalculator(MacRssiPair[] givenPairs){
-        Position[] givens = new Position[givenPairs.length];
+    public static Position fairPositionCalculator(MacRssiPair[] givenPairs){
+        int[] givens = new int[givenPairs.length];
+        for (int i = 0; i < givens.length; i++) {
+            givens[i] = givenPairs[i].getRssi();
+        }
         int validPositions=0;
         for (int i = 0; i < 5; i++) {
-            if (givens[i] != null &&
-                    givens[i].getX() >= 0 &&
-                    givens[i].getX() <= 200 &&
-                    givens[i].getY() >= 0  &&
-                    givens[i].getY() <= 200) {
+            if (givenPairs[i] != null) {
                 validPositions++;
-            } else {
-                givens[i] = null;
             }
         }
 
@@ -74,33 +72,28 @@ public class LocationCalculator {
             return new Position(-1,-1);
         }
 
-        double finX = 0;
-        double finY = 0;
-        for (int i = validPositions; i > 0; i--) {
-            if (givens[validPositions-i] != null){
-                finX += givens[validPositions-i].getX()*i;
-                finY += givens[validPositions-i].getY()*i;
-            }
+        double finX = Utils.getKnownLocations().get(givenPairs[0].getMac()).getX()*100.0;
+        double finY = Utils.getKnownLocations().get(givenPairs[0].getMac()).getY()*100.0;
+        double factor = 100.0;
+        double weigth;
+        double[] usedFactors = new double[validPositions];
+
+        for (int i = 1; i < validPositions; i++) {
+                Position current = Utils.getKnownLocations().get(givenPairs[validPositions-i].getMac());
+                weigth = factor / calculateFactor(givenPairs[0].getRssi(),givenPairs[i].getRssi());
+
+                finX += current.getX()*weigth;
+                finY += current.getY()*weigth;
+                usedFactors[i] = weigth;
         }
 
-        switch (validPositions) {
-            case 1: {
-                return new Position(finX, finY);
-            }
-            case 2: {
-                return new Position(finX/3.0, finY/3.0);
-            }
-            case 3: {
-                return new Position(finX/6.0,finY/6.0);
-            }
-            case 4: {
-                return new Position (finX/10.0,finY/10.0);
-            }
-            default: {
-                return new Position(finX/15.0,finY/15.0);
-            }
+        double totalFactors = 0;
+        for (int i = 0; i < usedFactors.length; i++) {
+            totalFactors += usedFactors[i];
         }
-    }*/
+
+        return new Position(finX/totalFactors,finY/totalFactors);
+    }
 
 
 
@@ -125,5 +118,12 @@ public class LocationCalculator {
         finY += newestCalculated.getY();
 
         return new Position(finX/ammountOfEntries, finY/ammountOfEntries);
+    }
+
+
+    public static double calculateFactor (int rSSI, int rSSII) {
+        int difference = rSSI-rSSII;
+        double result = Math.pow(2,Math.log10(difference)/Math.log10(3));
+        return result;
     }
 }
